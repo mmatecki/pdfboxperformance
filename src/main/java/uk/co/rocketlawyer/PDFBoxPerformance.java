30990d61b9a9e63deaffe3ab95c02be9dafa1f2c
Maciej Matecki <mmatecki@rocketlawyer.co.uk>
2017-11-06 11:06:51 +0000

    PDFBox performance testing

--- src/main/java/uk/co/rocketlawyer/PDFBoxPerformance.java
+++ src/main/java/uk/co/rocketlawyer/PDFBoxPerformance.java
@@ -6,6 +6,7 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import org.apache.pdfbox.rendering.PDFRenderer;
 
 public class PDFBoxPerformance {
@@ -15,24 +16,37 @@ public class PDFBoxPerformance {
     }
 
     void renderPages() {
-        int dpi = 150;
-        try (PDDocument document = getPDFDocument()) {
-            PDFRenderer renderer = new PDFRenderer(document);
-            for (int currentPage = 0; currentPage < document.getNumberOfPages(); currentPage++) {
-                LocalDateTime start = LocalDateTime.now();
-                BufferedImage bufferedImage = renderer.renderImageWithDPI(currentPage, dpi);
-                LocalDateTime finish = LocalDateTime.now();
-                System.out.println(String.format("Page %s rendered in %s ms ", currentPage, MILLIS.between(start, finish)));
-            }
+        int dpi = 600;
+
+        try (PDDocument document = getPDFDocument("/test-pdf-18.pdf")) {
+            renderPages(document, dpi);
+            System.out.println("---------------");
+        } catch (Exception e) {
+            System.out.println("Ups. " + e.getMessage());
+        }
+
+        try (PDDocument document = getPDFDocument("/test-2p.pdf")) {
+            renderPages(document, dpi);
+            System.out.println("---------------");
         } catch (Exception e) {
             System.out.println("Ups. " + e.getMessage());
         }
     }
 
-    private PDDocument getPDFDocument() throws IOException {
-        return PDDocument.load(getClass().getResourceAsStream("/test-2p.pdf"));
+    private void renderPages(PDDocument document, float dpi) throws IOException {
+        PDFRenderer renderer = new PDFRenderer(document);
+        for (int currentPage = 0; currentPage < document.getNumberOfPages(); currentPage++) {
+            LocalDateTime start = LocalDateTime.now();
+            BufferedImage bufferedImage = renderer.renderImageWithDPI(currentPage, dpi);
+            LocalDateTime finish = LocalDateTime.now();
+            System.out.println(
+                    String.format("Page %s rendered in %s ms ", currentPage, MILLIS.between(start, finish)));
+        }
     }
 
+    private PDDocument getPDFDocument(String file) throws IOException {
+        return PDDocument.load(getClass().getResourceAsStream(file));
+    }
 
 
 }
